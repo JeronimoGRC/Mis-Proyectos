62131ccc83ea505253c38a4790ef1e41743e33e0
JeronimoGRC <jeronimo.gr.castro@gmail.com>
2024-03-20 22:43:20 +0100

    20/03/2024

---
+++ Portfolio_Jeronimo/src/app/curiosidades/curiosidades.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-curiosidades',
+  standalone: true,
+  imports: [],
+  templateUrl: './curiosidades.component.html',
+  styleUrl: './curiosidades.component.scss'
+})
+export class CuriosidadesComponent {
+
+}
